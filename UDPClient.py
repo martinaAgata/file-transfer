f0879cd47f5e32c57f64f988ec73ce8080aa06ad
Martina Ágata <mpanetta@fi.uba.ar>
2022-09-17 15:08:46 -0300

    Basic implementation with one Client sending simple text file

--- UDPClient.py
+++ UDPClient.py
@@ -4,11 +4,21 @@ serverName = ''
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-message = input('Input lowercase sentence: ')
+message = input('Input filename: ')
 
-clientSocket.sendto(message.encode(),(serverName, serverPort))
+# Send filename.
+clientSocket.sendto(message.encode(), (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-
 print(modifiedMessage.decode())
 
+# Open file for sending using byte-array option.
+file = open(message, "rb")
+data = file.read()
+
+# Send file content.
+clientSocket.sendto(data,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
+
+file.close()
 clientSocket.close()
--- UDPServer.py
+++ UDPServer.py
@@ -6,8 +6,27 @@ serverSocket.bind(('', serverPort))
 
 print('The server is ready to receive')
 
-while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    print('Received from the Client: ' + modifiedMessage)
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+# Receive filename first.
+filename, clientAddress = serverSocket.recvfrom(2048)
+filename = filename.decode()
+print('Filename: ' + filename)
+
+# Send filename received ACK.
+serverSocket.sendto('Filename received.'.encode(), clientAddress)
+
+# Create new file where to put the content of the file to receive.
+# Opens a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
+file = open('copy-of-' + filename, 'w')
+
+# Receive file content.
+fileContent, clientAddress = serverSocket.recvfrom(2**30)
+print('Received file content from the Client.')
+
+# Send file content received ACK.
+serverSocket.sendto('File content received by the server.'.encode(), clientAddress)
+
+# Write file content to new file
+file.write(fileContent.decode())
+
+file.close()
+serverSocket.close()
