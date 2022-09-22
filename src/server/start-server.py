d94f32543ee2197f8a8f5c2ac95a657b349788bb
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-09-22 18:55:59 -0300

    Parse arguments for server

--- src/server/start-server.py
+++ src/server/start-server.py
@@ -1,9 +1,11 @@
+import argparse
 from socket import *
 import os
 
-serverPort = 12000
+DEFAULT_SERVER_PORT = 12000
 bufsize = 2048
-DIRPATH = 'files/'
+DEFAULT_DIRPATH = 'files/'
+
 
 def process_first_message(encodedFirstMessage):
 
@@ -11,11 +13,12 @@ def process_first_message(encodedFirstMessage):
 
     return (firstMessage[0], firstMessage[1])
 
+
 def recv_file(file, serverSocket):
     # Receive file content.
     maybeFileContent, clientAddress = serverSocket.recvfrom(bufsize)
 
-    #TODO: Think about a better way to end the transfer
+    # TODO: Think about a better way to end the transfer
     while maybeFileContent != "END".encode():
 
         # Write file content to new file
@@ -25,8 +28,8 @@ def recv_file(file, serverSocket):
         serverSocket.sendto('ACK'.encode(), clientAddress)
         maybeFileContent, clientAddress = serverSocket.recvfrom(bufsize)
 
-
     print('Received file content from the Client.')
+
 
 def send_file(file, serverSocket, clientAddress):
     data = file.read(bufsize)
@@ -44,6 +47,7 @@ def send_file(file, serverSocket, clientAddress):
     # inform the server that the download is finished
     serverSocket.sendto("END".encode(), clientAddress)
 
+
 def handle_upload_request(serverSocket, clientAddress, filename):
 
     # Send filename received ACK.
@@ -51,27 +55,29 @@ def handle_upload_request(serverSocket, clientAddress, filename):
 
     # Create new file where to put the content of the file to receive.
     # Opens a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
-    file = open(DIRPATH + filename, 'wb')
+    file = open(dirpath + filename, 'wb')
 
     recv_file(file, serverSocket)
 
     file.close()
-    
+
+
 def handle_download_request(serverSocket, clientAddress, filename):
 
-    if not os.path.exists(DIRPATH + filename):
+    if not os.path.exists(dirpath + filename):
         # Send filename does not exist NAK.
         serverSocket.sendto('NAK File does not exist.'.encode(), clientAddress)
         return
-    
+
     # Send filename received ACK.
     serverSocket.sendto('ACK Filename received.'.encode(), clientAddress)
 
-    file = open(DIRPATH + filename, 'rb')
+    file = open(dirpath + filename, 'rb')
 
     send_file(file, serverSocket, clientAddress)
 
     file.close()
+
 
 def listen(serverSocket):
     print('The server is ready to receive')
@@ -83,19 +89,60 @@ def listen(serverSocket):
         (command, filename) = process_first_message(firstMessage)
         print('Filename: ' + filename)
 
-        #TODO: Handle case where command is not upload and download 
+        # TODO: Handle case where command is not upload and download
         if command == 'upload':
             handle_upload_request(serverSocket, clientAddress, filename)
         else:
             handle_download_request(serverSocket, clientAddress, filename)
-            
+
+
+def parse_arguments():
+    argParser = argparse.ArgumentParser(
+        prog='start-server', description='Start the server with a specific configuration')
+
+    group = argParser.add_mutually_exclusive_group()
+    group.add_argument('-v', '--verbose',
+                       help='increase output verbosity', action='store_true')
+    group.add_argument('-q', '--quiet',
+                       help='decrease output verbosity', action='store_true')
+
+    argParser.add_argument('-H', '--host',
+                           type=str,
+                           help='service IP address',
+                           default='127.0.0.1',
+                           metavar='ADDR')
+    argParser.add_argument('-p', '--port',
+                           type=int,
+                           help='service port',
+                           default=DEFAULT_SERVER_PORT)
+    argParser.add_argument('-s', '--storage',
+                           type=str,
+                           help='storagte dir path',
+                           default=DEFAULT_DIRPATH,
+                           metavar='DIRPATH')
+
+    return argParser.parse_args()
+
+
 def start_server():
+    args = parse_arguments()
+
+    print("verbose", args.verbose)
+    print("quiet", args.quiet)
+    host = args.host
+    print("host", host)
+    port = args.port
+    print("port", port)
+    global dirpath
+    dirpath = args.storage
+    print("storage", dirpath)
+
     serverSocket = socket(AF_INET, SOCK_DGRAM)
-    serverSocket.bind(('', serverPort))
+    serverSocket.bind((host, port))
 
     # Create /files directory if not exist
-    if not os.path.isdir('files'):
-        os.mkdir('files')
+    if not os.path.isdir(dirpath):
+        os.mkdir(dirpath)
 
     listen(serverSocket)
 
@@ -105,8 +152,6 @@ def start_server():
 def main():
     start_server()
 
+
 if __name__ == "__main__":
     main()
-
-
-
