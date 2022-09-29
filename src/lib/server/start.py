d40f4eed93ff471718a02da376fb8f4784638f10
Martina Ágata <mpanetta@fi.uba.ar>
2022-09-29 13:03:28 -0300

    Linter fixes

--- src/lib/server/start.py
+++ src/lib/server/start.py
@@ -3,6 +3,8 @@ import logging
 from socket import socket, AF_INET, SOCK_DGRAM
 from lib.definitions import *
 from .argparser import parse_arguments
+from lib.client_handler import ClientHandler
+from lib.message import Message
 
 
 def process_first_message(encodedFirstMessage):
@@ -12,29 +14,37 @@ def process_first_message(encodedFirstMessage):
     return (firstMessage[0], firstMessage[1])
 
 
-def recv_file(file, serverSocket):
-    # Receive file content.
-    maybeFileContent, clientAddress = serverSocket.recvfrom(BUFSIZE)
+def recv_file(file, serverSocket, queue):
+    # Receive file content (should be first packet).
+    message = queue.get()
 
-    # TODO: Think about a better way to end the transfer
-    while maybeFileContent != "END".encode():
+    # TODO: Think about a better way to end the transfer.
+    # TODO: Perhaps it could stop iterating if type IS NOT DATA.
+    while message.type == DATA:
         logging.debug(
-            f"Received file content from client {clientAddress}")
+            f"Received file content from client {message.clientAddress}")
 
         # Write file content to new file
-        file.write(maybeFileContent)
+        file.write(message.data)
         logging.debug("File content written")
 
         # Send file content received ACK.
-        serverSocket.sendto('ACK'.encode(), clientAddress)
-        logging.debug(f"ACK sent to client {clientAddress}")
+        serverSocket.sendto('ACK'.encode(), message.clientAddress)
+        logging.debug(f"ACK sent to client {message.clientAddress}")
 
-        maybeFileContent, clientAddress = serverSocket.recvfrom(BUFSIZE)
+        message = queue.get()
 
-    logging.info(f"Received file from client {clientAddress}")
+    if message.type == FIN:
+        logging.info(f"Received file from client {message.clientAddress}")
+        serverSocket.sendto('FIN_ACK'.encode(), message.clientAddress)
+    else:
+        logging.info(f"ERROR: Received a {message.type} packet at the end of file upload")
+        # TODO: Check if sending FIN is the best choice.
+        serverSocket.sendto('FIN'.encode(), message.clientAddress)
+        
 
 
-def send_file(file, serverSocket, clientAddress):
+def send_file(file, serverSocket, clientAddress, queue):
     data = file.read(BUFSIZE)
 
     while data:
@@ -52,14 +62,14 @@ def send_file(file, serverSocket, clientAddress):
 
         data = file.read(BUFSIZE)
 
-    # inform the client that the download is finished
+    # Inform the client that the download is finished
     serverSocket.sendto("FIN".encode(), clientAddress)
     logging.debug(f"Sent FIN to client {clientAddress}")
 
     logging.info(f"Sent file to client {clientAddress}")
 
 
-def handle_upload_request(serverSocket, clientAddress, filename):
+def handle_upload_request(clientAddress, serverSocket, queue, dirpath, filename):
     logging.info("Handling upload request")
 
     # Send filename received ACK.
@@ -72,12 +82,11 @@ def handle_upload_request(serverSocket, clientAddress, filename):
     file = open(dirpath + filename, 'wb')
     logging.debug(f"File to write in is {dirpath}/{filename}")
 
-    recv_file(file, serverSocket)
+    recv_file(file, serverSocket, queue)
 
     file.close()
 
-
-def handle_download_request(serverSocket, clientAddress, filename):
+def handle_download_request(clientAddress, serverSocket, queue, dirpath, filename):
     logging.info("Handling download request")
 
     if not os.path.exists(dirpath + filename):
@@ -96,7 +105,7 @@ def handle_download_request(serverSocket, clientAddress, filename):
     file = open(dirpath + filename, 'rb')
     logging.debug(f"File to read from is {dirpath}/{filename}")
 
-    send_file(file, serverSocket, clientAddress)
+    send_file(file, serverSocket, clientAddress, queue)
 
     file.close()
 
@@ -104,24 +113,63 @@ def handle_download_request(serverSocket, clientAddress, filename):
 def listen(serverSocket, dirpath):
     logging.info("Socket created and listening for requests")
 
+    clientHandler = None
+    clientsDict = {}
+
     while True:
-        # Receive filepath first.
-        firstMessage, clientAddress = serverSocket.recvfrom(BUFSIZE)
+        try:
+            # Receive filepath first.
+            firstMessage, clientAddress = serverSocket.recvfrom(BUFSIZE)
 
-        logging.debug(f"First message received from {clientAddress}")
-        logging.debug(f"First message content: {firstMessage}")
-        (command, filename) = process_first_message(firstMessage)
-        logging.info(f"Received {command} command for file {filename}")
+            message = Message(firstMessage, clientAddress)
 
-        # TODO: Handle case where command is not upload and download
-        if command == UPLOAD:
-            handle_upload_request(serverSocket, clientAddress, filename)
-        elif command == DOWNLOAD:
-            handle_download_request(serverSocket, clientAddress, filename)
-        else:
-            logging.error(
-                f"Received an invalid command from client {clientAddress}")
+            # Get the clientHanlder
+            if clientAddress in clientsDict:
+                # OLD CLIENT
+                clientHandler = clientsDict[clientAddress]
+            else:
+                # NEW CLIENT
+                # TODO: maybe check if file exists or NAK
+                if message.type in [UPLOAD, DOWNLOAD]:
+                    clientHandler = ClientHandler(
+                        clientAddress,
+                        serverSocket,
+                        dirpath)
+                    clientHandler.start_thread()
+                    clientsDict[clientAddress] = clientHandler
+                else:
+                    # TODO: log me
+                    # TODO: send reasons for NAK
+                    serverSocket.sendto(NAK.encode(), clientAddress)
+                    continue
 
+            # Send the message to the clientHandler
+            clientHandler.send(message)
+
+            # TODO: Check that every time that a FIN is send, the thread go to
+            # the end of the scope.
+            #
+            # If FIN -> Join client, and send FIN_ACK
+            # If FIN_ACK -> We've already sent FIN, so just join client
+            # In both cases, we must remove clientHandler from clientsDict
+            if message.type in [FIN, FIN_ACK]:
+                clientHandler.join()
+                logging.debug(f"Client {clientAddress} thread was joined")
+                del clientsDict[clientAddress]
+                logging.debug(f"Client {clientAddress} has been disconnected")
+        except KeyboardInterrupt:
+            logging.debug("A keyboard interrupt signal has been received")
+            print()
+            break
+
+    # This code is unreachable until we set ctrl+c signal
+    for (clientAddress, clientHandler) in clientsDict:
+        message = Message("FIN".encode(), clientAddress)
+        clientHandler.send(message)
+        clientHandler.join()
+        logging.debug(f"Client {clientAddress} thread was joined")
+
+    clientsDict.clear()
 
 
 def start_server():
@@ -152,4 +200,3 @@ def start_server():
 
     serverSocket.close()
     logging.info("Socket closed")
-
