bd6265ee94d4366bb25c010162c38cfe33803bce
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-10-02 22:20:52 -0300

    Comment added due to possible bug

--- src/lib/StopAndWait.py
+++ src/lib/StopAndWait.py
@@ -3,6 +3,7 @@ from .message import Message
 from .definitions import (BUFSIZE, UPLOAD, DOWNLOAD, DATA,
                           FIN, FIN_ACK, ACK, NAK, TIMEOUT)
 
+
 class StopAndWait:
     def __init__(self, transferMethod):
         self.bit = 0
@@ -11,6 +12,7 @@ class StopAndWait:
     def alternateBit(self):
         self.bit = 1 - self.bit
 
+    # Returns a Message received (that is not duplicated) from the other part.
     def receive(self, address=None, lastSentMsg=None, lastSentBit=None, lastRcvBit=None, timeout=None):
         # TODO: change 5 to a constant
         for _ in range(0, 5):
@@ -21,33 +23,43 @@ class StopAndWait:
 
                 # If it's the first message, then None is set, so the while condition is always false
                 while lastRcvBit == message.bit:
-                    logging.debug(f"Duplicated package with bit = {message.bit} and {message.data[:15]}")
+                    logging.debug(
+                        f"Duplicated package with bit = {message.bit} and {message.data[:15]}")
                     # The recv sends another ACK msg, but the sender do nothing
                     if not timeout:
-                        self.transferMethod.sendMessage(lastSentBit, lastSentMsg, address)
+                        self.transferMethod.sendMessage(
+                            lastSentBit, lastSentMsg, address)
 
                     message = self.transferMethod.recvMessage(timeout)
 
-                logging.debug(f"Message received from {message.clientAddress} successfully")
+                logging.debug(
+                    f"Message received from {message.clientAddress} successfully")
                 return message
 
+            # Message retransmission in case of timeout
             except Exception as err:
                 # When sender throw a timeout exception, we re-send the message, at least until <CONSTANT> times
-                logging.debug(f"Message queue raise timeout exception while waiting for receiving message: {format(err)}")
-                self.transferMethod.sendMessage(lastSentBit, lastSentMsg, address)
+                logging.debug(
+                    f"Message queue raise timeout exception while waiting for receiving message: {format(err)}")
+                self.transferMethod.sendMessage(
+                    lastSentBit, lastSentMsg, address)
 
+        # TODO: possible BUG. If a 6th timeout occurrs, then it is NOT catched and the application might close badly
         message = self.transferMethod.recvMessage(timeout)
 
         # If it's the first message, then None is set, so the while condition is always false
         while lastRcvBit == message.bit:
-            logging.debug(f"Duplicated package with bit = {message.bit} and {message.data[:15]}")
+            logging.debug(
+                f"Duplicated package with bit = {message.bit} and {message.data[:15]}")
             # The recv sends another ACK msg, but the sender do nothing
             if not timeout:
-                self.transferMethod.sendMessage(lastSentBit, lastSentMsg, address)
+                self.transferMethod.sendMessage(
+                    lastSentBit, lastSentMsg, address)
 
             message = self.transferMethod.recvMessage(timeout)
 
-        logging.debug(f"Message received from {message.clientAddress} successfully")
+        logging.debug(
+            f"Message received from {message.clientAddress} successfully")
         return message
 
     def send_file(self, file, address, timeout):
@@ -58,19 +70,27 @@ class StopAndWait:
                 logging.debug("Read data from file")
 
                 self.transferMethod.sendMessage(self.bit, data, address)
-                logging.debug(f"Sent data to {address}")   
+                logging.debug(f"Sent data to {address}")
 
-                message = self.receive(address, lastSentMsg=data, lastSentBit=self.bit, timeout=timeout)
+                # TODO: BUG no deberíamos pasarle al receive el lastRecvBit acá? porque así esperamos el
+                # ACK correcto (nos puede llegar un ACK duplicado proveniente
+                # de que le mandamos un duplicado al receiver por un timeout)
+                message = self.receive(
+                    address, lastSentMsg=data, lastSentBit=self.bit, timeout=timeout)
                 self.alternateBit()
 
                 if message.type != ACK:
                     if message.type == FIN:
-                        logging.info(f"{FIN} messsage received from {address}.")
-                        self.transferMethod.sendMessage(self.bit, FIN_ACK.encode(), address)
+                        logging.info(
+                            f"{FIN} messsage received from {address}.")
+                        self.transferMethod.sendMessage(
+                            self.bit, FIN_ACK.encode(), address)
                         logging.debug(f"{FIN_ACK} messsage sent to {address}.")
                     else:
-                        logging.error(f"Unknown message received: {message.data[:15]}, from {address}")
-                        self.transferMethod.sendMessage(self.bit, FIN.encode(), address)
+                        logging.error(
+                            f"Unknown message received: {message.data[:15]}, from {address}")
+                        self.transferMethod.sendMessage(
+                            self.bit, FIN.encode(), address)
                         logging.info(f"{FIN} messsage sent to {address}.")
                     logging.error("File transfer NOT completed")
                     return
@@ -82,7 +102,8 @@ class StopAndWait:
             self.transferMethod.sendMessage(self.bit, FIN.encode(), address)
             logging.info(f"{FIN} messsage sent to {address}.")
 
-            _ = self.receive(address, lastSentMsg=FIN.encode(), lastSentBit=self.bit, timeout=timeout)
+            _ = self.receive(address, lastSentMsg=FIN.encode(),
+                             lastSentBit=self.bit, timeout=timeout)
             logging.info("File transfer completed")
 
         except Exception as err:
@@ -90,7 +111,8 @@ class StopAndWait:
                 f"An error occurred when sending file: {format(err)}")
 
     def recv_file(self, file, address, lastSentMsg=None, lastRcvBit=None):
-        message = self.receive(address, lastSentMsg=lastSentMsg, lastRcvBit=lastRcvBit)
+        message = self.receive(
+            address, lastSentMsg=lastSentMsg, lastRcvBit=lastRcvBit)
 
         while message.type == DATA:
             logging.debug(
@@ -102,19 +124,24 @@ class StopAndWait:
 
             # Send file content received ACK.
             raw_message_to_send = ACK.encode()
-            self.transferMethod.sendMessage(message.bit, raw_message_to_send, message.clientAddress)
+            self.transferMethod.sendMessage(
+                message.bit, raw_message_to_send, message.clientAddress)
             logging.debug(f"{ACK} sent to {message.clientAddress}")
             message = self.receive(message.clientAddress, lastSentMsg=raw_message_to_send,
-                                                                     lastRcvBit=message.bit)
+                                   lastRcvBit=message.bit)
 
         if message.type == FIN:
-            logging.info(f"{FIN} messsage received from {message.clientAddress}.")
-            self.transferMethod.sendMessage(message.bit, FIN_ACK.encode(), message.clientAddress)
-            logging.debug(f"{FIN_ACK} messsage sent to {message.clientAddress}.")
+            logging.info(
+                f"{FIN} messsage received from {message.clientAddress}.")
+            self.transferMethod.sendMessage(
+                message.bit, FIN_ACK.encode(), message.clientAddress)
+            logging.debug(
+                f"{FIN_ACK} messsage sent to {message.clientAddress}.")
             logging.info(f"Received file from {message.clientAddress}")
         else:
-            logging.error(f"Unknown message received {message.data[:15]} from {message.clientAddress}")
-            self.transferMethod.sendMessage(message.bit, FIN.encode(), message.clientAddress)
+            logging.error(
+                f"Unknown message received {message.data[:15]} from {message.clientAddress}")
+            self.transferMethod.sendMessage(
+                message.bit, FIN.encode(), message.clientAddress)
             logging.debug(f"{FIN} messsage sent to {message.clientAddress}.")
             logging.info("File transfer NOT completed")
-
